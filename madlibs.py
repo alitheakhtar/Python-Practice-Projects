59a095733fa7cd94b6d115747524305aa9ad60d3
ali_akhtar <aakhtar07@gmail.com>
2021-11-25 11:21:28 +0000

    First Project thanks to https://youtu.be/8ext9G7xspg

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+# string concatenation
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person:  ")
+
+madLib = f'computer programming is so {adj}! it makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}'
+
+print(madLib)
